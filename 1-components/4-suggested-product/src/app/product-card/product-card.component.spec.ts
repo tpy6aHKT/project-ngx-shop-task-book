531368ec9d569279eb223e01a61c48f56108f67a
Ihor <igornepipenko@gmail.com>
2020-09-03 13:53:05 +0300

    Feature: first version for 1-components [ref:no-ref]

--- 1-components/4-suggested-product/src/app/product-card/product-card.component.spec.ts
+++ 1-components/4-suggested-product/src/app/product-card/product-card.component.spec.ts
@@ -1,74 +1,86 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ProductCardComponent } from './product-card.component';
-import { productData } from '../../mocks/mock-product';
 import { By } from '@angular/platform-browser';
+import { EventEmitter } from '@angular/core';
+import { oneProduct } from '../../../../../shared/mocks/1-components/product';
 
 describe('[Moдуль 1]  Компонент рекомендуемого товара', () => {
   let fixture: ComponentFixture<ProductCardComponent>;
   let component: ProductCardComponent;
-  let goToProductSpy: jasmine.Spy;
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [ProductCardComponent],
     });
     fixture = TestBed.createComponent(ProductCardComponent);
     component = fixture.componentInstance;
-    (component as any).product = productData;
     fixture.detectChanges();
-
-    spyOn(component as any, 'redirectTo').and.callThrough();
-    goToProductSpy = spyOn(
-      (component as any).goToProduct,
-      'emit'
-    ).and.callThrough();
   });
-  it('компонент должен иметь метод redirectTo и Output свойства goToProduct', () => {
-    expect((component as any).goToProduct).toBeTruthy();
+
+  it('компонент должен иметь метод redirectTo ', () => {
     expect((component as any).redirectTo).toBeTruthy();
   });
 
-  it('компонент должен иметь свойство product и декоратор Input этого свойства', () => {
-    expect(component.hasOwnProperty('product')).toBeTruthy();
-  });
-  it('тег с селектором .card-title должен правильно интерполировать имя товара', () => {
-    const titleEL = fixture.debugElement.query(By.css('.card-title'));
-    expect(titleEL).toBeTruthy();
-    const { name } = (component as any)?.product;
-    const [{ nativeNode: titleNode }] = titleEL.childNodes;
-    expect(titleNode.textContent.trim()).toEqual(name);
-  });
-  it('тег img должен иметь правильное связывание свойств src через пайп и alt', () => {
-    const imgEL = fixture.debugElement.query(By.css('.card-img-top'));
-    expect(imgEL).toBeTruthy();
-    const { name } = (component as any)?.product;
-    const src = (component as any)?.product.images[0].url;
-    expect(imgEL.attributes.src).toEqual(src);
-    expect(imgEL.attributes.alt).toEqual(name);
+  it('компонент должен иметь собственное событие goToProduct ', () => {
+    expect((component as any).goToProduct).toBeTruthy();
+    expect((component as any).goToProduct).toBeInstanceOf(EventEmitter);
   });
 
-  it('тег с селектором .rate-amount должен правильно интерполировать количество отзывов к товара', () => {
-    const rateEL = fixture.debugElement.query(By.css('.rate-amount'));
-    expect(rateEL).toBeTruthy();
-    const { feedbacksCount } = (component as any)?.product;
-    const [{ nativeNode: rateNode }] = rateEL.childNodes;
-    expect(Number(rateNode.textContent.substring(0, 2).trim())).toEqual(
-      feedbacksCount
-    );
-  });
-  it('тег с селектором .price-text должен правильно интерполировать цену товара', () => {
-    const titleEL = fixture.debugElement.query(By.css('.price-text'));
-    expect(titleEL).toBeTruthy();
-    const { price } = (component as any)?.product;
-    const [{ nativeNode: titleNode }] = titleEL.childNodes;
-    expect(Number(titleNode.textContent.slice(1).trim())).toEqual(price);
+  it('компонент должен иметь свойство product c значением {}', () => {
+    expect((component as any).product).toBeDefined();
   });
 
-  it('клик на компонент должен вызывать метод redirectTo()', () => {
-    const icon = fixture.debugElement.query(By.css('.card-component'));
-    icon.triggerEventHandler('click', null);
-    expect((component as any).redirectTo).toHaveBeenCalledBefore(
-      goToProductSpy
-    );
-    expect((component as any).goToProduct.emit).toHaveBeenCalled();
+  it('при нажатии на блок с селектором .go-to-product должен вызываться метод  redirectTo и срабатывать собстевнное событие goToProduct',
+    () => {
+      spyOn(component as any, 'redirectTo').and.callThrough();
+      spyOn((component as any)?.goToProduct, 'emit').and.callThrough();
+      const incrementButton = fixture.debugElement.query(By.css('div.go-to-product'));
+      incrementButton.triggerEventHandler('click', null);
+      expect((component as any)?.redirectTo).toHaveBeenCalledTimes(1);
+      expect((component as any)?.goToProduct.emit).toHaveBeenCalledTimes(1);
+    });
+
+  it('тег c селекторор [.card-img-wrap img] должен иметь правильное связывание свойств src и alt', () => {
+    (component as any).product = oneProduct;
+    fixture.detectChanges();
+    const imgEl = fixture.debugElement.query(By.css('.card-img-wrap img'));
+    expect(imgEl).toBeTruthy();
+    const {
+      image,
+      name,
+    } = (component as any)?.product;
+    console.log((component as any)?.product);
+    expect(imgEl.attributes.src).toEqual(image);
+    expect(imgEl.attributes.alt).toEqual(name);
   });
+
+  it('тег с селектором .card-title  должен правильно интерполировать свойство name продукта', () => {
+    (component as any).product = oneProduct;
+    fixture.detectChanges();
+    const prodNameEL = fixture.debugElement.query(By.css('.card-title'));
+    expect(prodNameEL).toBeTruthy();
+    expect(prodNameEL.nativeElement.textContent.trim()).toEqual(
+      (component as any)?.product.name
+    );
+  });
+
+  it('тег с селектором .rate .rate-amount  должен правильно интерполировать свойство feedbacksCount продукта', () => {
+    (component as any).product = oneProduct;
+    fixture.detectChanges();
+    const prodNameEL = fixture.debugElement.query(By.css('.rate .rate-amount'));
+    expect(prodNameEL).toBeTruthy();
+    expect(prodNameEL.nativeElement.textContent.trim()).toEqual(
+      `${(component as any)?.product.feedbacksCount} отзыва`
+    );
+  });
+
+  it('тег с селектором .product-price strong  должен правильно интерполировать свойство price продукта', () => {
+    (component as any).product = oneProduct;
+    fixture.detectChanges();
+    const prodNameEL = fixture.debugElement.query(By.css('.product-price strong'));
+    expect(prodNameEL).toBeTruthy();
+    expect(prodNameEL.nativeElement.textContent.trim()).toEqual(
+      `${(component as any)?.product.price}€`
+    );
+  });
+
 });
--- 1-components/4-suggested-product/src/app/product-card/product-card.component.ts
+++ 1-components/4-suggested-product/src/app/product-card/product-card.component.ts
@@ -1,20 +1,9 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { IProduct } from '../../../../../shared/mocks/1-components/product';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'ngx-shop-product-card',
   templateUrl: './product-card.component.html',
 })
-// @ts-ignore
 export class ProductCardComponent {
-  @Input()
-  // @ts-ignore
-  public product!: IProduct = {} as IProduct;
-  @Output()
-  // @ts-ignore
-  public goToProduct: EventEmitter<void> = new EventEmitter<void>();
 
-  public redirectTo(): void {
-    this.goToProduct.emit();
-  }
 }
