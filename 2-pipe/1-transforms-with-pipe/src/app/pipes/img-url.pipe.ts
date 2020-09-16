794f5fc43a751720f4af31cc63dc3c239698f661
Ihor <igornepipenko@gmail.com>
2020-09-16 21:39:03 +0300

    Fix: remove src for pipes [ref:no-ref]

--- 2-pipe/1-transforms-with-pipe/src/app/pipes/img-url.pipe.ts
+++ 2-pipe/1-transforms-with-pipe/src/app/pipes/img-url.pipe.ts
@@ -7,14 +7,6 @@ import { IProductImage } from '../../../../../shared/mocks/2-pipes/product';
 // @ts-ignore
 export class ImgUrlPipe implements PipeTransform {
   public transform(images: IProductImage[] | undefined): string {
-    if (!Array.isArray(images)) {
-      return '';
-    }
-    const [currentImg] = images;
-    if (!currentImg) {
-      return '';
-    }
-
-    return `${currentImg?.url || ''}`;
+    return '';
   }
 }
--- 2-pipe/1-transforms-with-pipe/src/app/pipes/rate.pipe.ts
+++ 2-pipe/1-transforms-with-pipe/src/app/pipes/rate.pipe.ts
@@ -6,16 +6,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 // @ts-ignore
 export class RatePipe implements PipeTransform {
   public transform(value: number): number | undefined {
-    if (value - Math.trunc(value) < 0.25) {
-      value = Math.trunc(value);
-    } else if (
-      value - Math.trunc(value) >= 0.25 &&
-      value - Math.trunc(value) < 0.75
-    ) {
-      value = Math.trunc(value) + 0.5;
-    } else {
-      value = Math.trunc(value) + 1;
-    }
-    return value;
+    return 0;
   }
 }
