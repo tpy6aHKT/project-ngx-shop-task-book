4020341abbaa4ab3adda5a1217a59cf67c41258a
prysiazhna <olha.prysiazhna@gmail.com>
2020-09-22 18:49:47 +0300

    feature: added tests for information, side-menu components [ref: no-ref]

--- 3-directives/3-information-component/src/app/information/information.component.ts
+++ 3-directives/3-information-component/src/app/information/information.component.ts
@@ -7,14 +7,14 @@ import { IProduct } from '../../../../../shared/mocks/3-directives/product-infor
 })
 export class InformationComponent {
   @Input()
-  public product?: IProduct;
+  public product?: IProduct = null;
   public isShow = false;
 
   @Output()
   public addToCart: EventEmitter<string> = new EventEmitter<string>();
 
   public addToBasket(): void {
-    this.addToCart.emit('товар добавлен в карзину');
+    this.addToCart.emit('товар добавлен в кoрзину');
   }
   public show(): void {
     this.isShow = !this.isShow;
