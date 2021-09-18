1d965613c1294bbac0e26f9d6e9b287c07256fcd
tpy6ahkt <tpy6ahkt@gmail.com>
2021-09-19 00:06:43 +0500

    fix(no-ref): 1.1 shop-card done

--- 1-components/1-shop-card/src/app/shop-card/shop-card.component.ts
+++ 1-components/1-shop-card/src/app/shop-card/shop-card.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { ICartProduct } from '../../../../../shared/mocks/1-components/cart-product';
 
 @Component({
@@ -9,4 +9,18 @@ import { ICartProduct } from '../../../../../shared/mocks/1-components/cart-prod
 export class ShopCardComponent {
 	@Input()
 	public product: ICartProduct = {} as ICartProduct;
+
+	@Output()
+	public increment: EventEmitter<void> = new EventEmitter<void>();
+
+	@Output()
+	public decrement: EventEmitter<void> = new EventEmitter<void>();
+
+	public incrementProductInCart() {
+		this.increment.emit();
+	}
+
+	public decrementProductInCart() {
+		this.decrement.emit();
+	}
 }
